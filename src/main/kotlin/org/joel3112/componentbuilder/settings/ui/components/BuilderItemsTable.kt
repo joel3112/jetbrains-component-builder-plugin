1424b3e01b28dff85dfe2e752a349845c10c0ee2
Joel Paucarima Franco <joel.dev.3112@gmail.com>
2024-03-30 11:04:14 +0100

    add hints for icons and parent indicator

--- src/main/kotlin/org/joel3112/componentbuilder/settings/ui/components/BuilderItemsTable.kt
+++ src/main/kotlin/org/joel3112/componentbuilder/settings/ui/components/BuilderItemsTable.kt
@@ -1,16 +1,21 @@
 package org.joel3112.componentbuilder.settings.ui.components
 
 import com.intellij.execution.util.ListTableWithButtons
+import com.intellij.icons.AllIcons
 import com.intellij.openapi.actionSystem.ActionToolbarPosition
 import com.intellij.openapi.observable.properties.ObservableMutableProperty
 import com.intellij.openapi.observable.util.transform
 import com.intellij.ui.ToolbarDecorator
 import com.intellij.util.ui.ColumnInfo
+import com.intellij.util.ui.JBUI
 import com.intellij.util.ui.ListTableModel
 import org.joel3112.componentbuilder.settings.data.Item
 import org.joel3112.componentbuilder.settings.data.SettingsService
+import java.awt.Component
+import javax.swing.JTable
 import javax.swing.ListSelectionModel
 import javax.swing.SwingUtilities
+import javax.swing.table.DefaultTableCellRenderer
 
 class BuilderItemsTable(private val settingsProperty: ObservableMutableProperty<SettingsService>) :
     ListTableWithButtons<Item>() {
@@ -31,6 +36,11 @@ class BuilderItemsTable(private val settingsProperty: ObservableMutableProperty<
         tableView.apply {
             columnSelectionAllowed = false
             tableHeader.reorderingAllowed = false
+            rowHeight = JBUI.scale(24)
+            columnModel.getColumn(0).apply {
+                maxWidth = JBUI.scale(22)
+                minWidth = JBUI.scale(22)
+            }
             model.addTableModelListener {
                 itemsProperty.set(elements)
             }
@@ -49,7 +59,7 @@ class BuilderItemsTable(private val settingsProperty: ObservableMutableProperty<
 
     override fun isUpDownSupported() = true
     override fun shouldEditRowOnCreation() = false
-    override fun createListModel(): ListTableModel<*> = ListTableModel<Item>(NameColumn())
+    override fun createListModel(): ListTableModel<*> = ListTableModel<Item>(IconColumn(), NameColumn())
     override fun createElement() = Item()
     override fun addNewElement(newElement: Item) {
         super.addNewElement(newElement)
@@ -62,8 +72,50 @@ class BuilderItemsTable(private val settingsProperty: ObservableMutableProperty<
     override fun cloneElement(variable: Item) = variable.copy()
     override fun isEmpty(element: Item): Boolean = element.name.isEmpty()
 
+    private class IconColumn : ColumnInfo<Item, String>("") {
+        override fun getName() = ""
+        override fun valueOf(item: Item?) = ""
+        override fun getRenderer(item: Item?) = object : DefaultTableCellRenderer() {
+            override fun getTableCellRendererComponent(
+                table: JTable?,
+                value: Any?,
+                isSelected: Boolean,
+                hasFocus: Boolean,
+                row: Int,
+                column: Int
+            ): Component {
+                return super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column)
+                    .apply {
+                        if (item != null && item.icon.isNotEmpty()) {
+                            icon = item.icon.let {
+                                AllIcons.FileTypes::class.java.getField(it).get(null)
+                            } as javax.swing.Icon
+                        }
+                    }
+            }
+        }
+    }
+
     private class NameColumn : ColumnInfo<Item, String>("") {
+        override fun getName() = ""
         override fun valueOf(item: Item?) = item?.name
+        override fun getRenderer(item: Item?) = object : DefaultTableCellRenderer() {
+            override fun getTableCellRendererComponent(
+                table: JTable?,
+                value: Any?,
+                isSelected: Boolean,
+                hasFocus: Boolean,
+                row: Int,
+                column: Int
+            ): Component {
+                return super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column)
+                    .apply {
+                        if (item != null) {
+                            font = font.deriveFont(if (!item.isChildFile) java.awt.Font.BOLD else java.awt.Font.PLAIN)
+                        }
+                    }
+            }
+        }
     }
 
 }
