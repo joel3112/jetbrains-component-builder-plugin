f9fb55b4f2e704601fc139999a40bbb9f9c7678b
Joel Paucarima Franco <joel.dev.3112@gmail.com>
2024-08-29 21:55:12 +0200

    sync nodes in tree

--- src/main/kotlin/org/joel3112/componentbuilder/settings/ui/components/BuilderItemTree.kt
+++ src/main/kotlin/org/joel3112/componentbuilder/settings/ui/components/BuilderItemTree.kt
@@ -2,7 +2,7 @@ package org.joel3112.componentbuilder.settings.ui.components
 
 import com.intellij.icons.AllIcons
 import com.intellij.openapi.actionSystem.ActionToolbarPosition
-import com.intellij.openapi.observable.properties.ObservableMutableProperty
+import com.intellij.openapi.observable.properties.GraphProperty
 import com.intellij.openapi.observable.util.transform
 import com.intellij.ui.AnActionButton
 import com.intellij.ui.AnActionButtonRunnable
@@ -19,6 +19,7 @@ import javax.swing.JTree
 import javax.swing.tree.*
 
 private const val ROOT_NAME = "ROOT"
+
 private class ItemTreeCellRenderer : DefaultTreeCellRenderer() {
     override fun getTreeCellRendererComponent(
         tree: JTree?,
@@ -36,7 +37,7 @@ private class ItemTreeCellRenderer : DefaultTreeCellRenderer() {
         val node = value as DefaultMutableTreeNode
         if (node.userObject is Item) {
             val item = node.userObject as Item
-            val isNodeParent = node.parent.toString() == ROOT_NAME
+            val isNodeParent = node.parent?.toString() == ROOT_NAME
 
             text = item.name
             if (isNodeParent) {
@@ -57,7 +58,7 @@ private class ItemTreeCellRenderer : DefaultTreeCellRenderer() {
 }
 
 
-class BuilderItemTree(private val settingsProperty: ObservableMutableProperty<SettingsService>) : Tree() {
+class BuilderItemTree(private val settingsProperty: GraphProperty<SettingsService>) : Tree() {
     private var myDecorator: ToolbarDecorator
     private var root: DefaultMutableTreeNode = DefaultMutableTreeNode(ROOT_NAME)
 
@@ -134,7 +135,7 @@ class BuilderItemTree(private val settingsProperty: ObservableMutableProperty<Se
 
 
     private fun createRemoveAction(): AnActionButtonRunnable {
-        return AnActionButtonRunnable { button -> {} }
+        return AnActionButtonRunnable { button -> removeSelectedNode() }
     }
 
     private fun createAddAction(): AnActionButtonRunnable {
@@ -158,13 +159,70 @@ class BuilderItemTree(private val settingsProperty: ObservableMutableProperty<Se
         val root = model.root as DefaultMutableTreeNode
         val node = DefaultMutableTreeNode(Item())
         root.add(node)
-        refreshValues(node)
+        itemsProperty.set(treeItems)
+        selectNodeOrLastNode(node)
     }
 
-    private fun refreshValues(node: DefaultMutableTreeNode?) {
+    private fun removeSelectedNode() {
+        val selectedNode = lastSelectedPathComponent as DefaultMutableTreeNode
+        val parent = selectedNode.parent as DefaultMutableTreeNode
+        parent.remove(selectedNode)
+
         itemsProperty.set(treeItems)
+        selectNodeOrLastNode(null)
+    }
+
+    private fun selectNodeOrLastNode(node: DefaultMutableTreeNode?) {
         if (node != null) {
             selectionPath = TreePath(node.path)
+            return
+        }
+        val lastNode = root.children().toList().lastOrNull() as DefaultMutableTreeNode?
+        if (lastNode != null) {
+            selectionPath = TreePath(lastNode.path)
+        }
+    }
+
+    private fun findNode(item: Item): DefaultMutableTreeNode? {
+        val rootNode = model.root as DefaultMutableTreeNode
+        return rootNode.breadthFirstEnumeration().asSequence().map { it as DefaultMutableTreeNode }
+            .find { node ->
+                val selectedObject = node.userObject
+                if (selectedObject is Item) {
+                    (node.userObject as Item).id == item.id
+                } else {
+                    false
+                }
+            }
+    }
+
+    private fun refreshNode(item: Item) {
+        val node = findNode(item)
+        if (node != null) {
+            node.userObject = item
+            (model as DefaultTreeModel).nodeChanged(node)
+        }
+    }
+
+    fun syncNodes() {
+        updateUI()
+        println("refreshNodes = settingsProperty:${settingsProperty.get().items.size} - treeItems:${treeItems.size}")
+
+        if (settingsProperty.get().items.size != treeItems.size) {
+            val settingsItemIds = settingsProperty.get().items.map { it.id }.toSet()
+            val nodesToRemove = treeItems
+                .filter { it.id !in settingsItemIds }
+                .map { findNode(it) }
+            nodesToRemove.forEach { node ->
+                val parent = node?.parent as DefaultMutableTreeNode
+                parent.remove(node)
+            }
+            selectNodeOrLastNode(null)
+            return
+        }
+
+        settingsProperty.get().items.forEach { item ->
+            refreshNode(item)
         }
     }
 }
