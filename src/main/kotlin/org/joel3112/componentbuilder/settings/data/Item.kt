cb904e567374b9123fc22e5b020995942f459550
Joel Paucarima Franco <joel.dev.3112@gmail.com>
2024-09-10 00:27:31 +0200

    add duplicate action

--- src/main/kotlin/org/joel3112/componentbuilder/settings/data/Item.kt
+++ src/main/kotlin/org/joel3112/componentbuilder/settings/data/Item.kt
@@ -7,9 +7,12 @@ import net.pearx.kasechange.toPascalCase
 import org.joel3112.componentbuilder.utils.toReactHookCase
 import java.util.*
 
+const val DEFAULT_NAME = "Unnamed"
+val createDefaultId = { UUID.randomUUID().toString() }
+
 data class Item(
     @get:OptionTag("ID")
-    var id: String = UUID.randomUUID().toString(),
+    var id: String = createDefaultId(),
 
     @get:OptionTag("ENABLED")
     var enabled: Boolean = true,
@@ -18,7 +21,7 @@ data class Item(
     var parent: String = "",
 
     @get:OptionTag("NAME")
-    var name: String = "Unnamed",
+    var name: String = DEFAULT_NAME,
 
     @get:OptionTag("FILE_PATH")
     var filePath: String = "",
